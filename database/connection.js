8cbdcf76dcf613ef53c745d63dc357cfb313d31c
Benjamin Kao <benjamin.kao00@gmail.com>
2021-03-10 15:34:52 -0800

    Add SSL to connection.js

--- database/connection.js
+++ database/connection.js
@@ -1,5 +1,5 @@
 const pgp = require('pg-promise')();
 
-const connection = pgp(process.env.DATABASE_URL + "sslrequire=true");
+const connection = pgp(process.env.DATABASE_URL + "?sslmode=require");
 
 module.exports = connection;
