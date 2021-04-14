d2d009784dad954eec39c8be5a904a1826079ac8
Doxify <ageorgescu@mail.sfsu.edu>
2021-04-14 13:28:26 -0700

    implemented ActiveRecord class

---
+++ database/ActiveRecord.js
@@ -0,0 +1,35 @@
+import db from './connection';
+
+class ActiveRecord {
+    table_name = "";
+    fields = [];
+
+    constructor(table_name, fields) {
+        this.table_name = table_name;
+        this.fields = fields;
+    }
+
+    static all() {
+        return db.any(`SELECT * FROM ${table_name}`);
+    }
+
+    static find(id) {
+        return db.oneOrNone(`SELECT * FROM ${table_name} WHERE id=$id`, { id });
+    }
+
+    static create(data) {
+        const fieldsAndCols = [[], []]
+        
+        Object.entries(data).forEach(([key, value]) => {
+            if (!fields.includes(key)) {
+                throw new Error("Data contains invalid field.");
+            }
+            db_data[0].push(key);
+            db_data[1].push(value);
+        });
+
+        return db.none(`INSERT INTO ${table_name}(${fieldsAndCols[0].join(", ")}) VALUES (${fieldsAndCols[1].join(", ")})`)
+    }
+}
+
+module.exports = ActiveRecord;
