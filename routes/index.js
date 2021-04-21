3e5c530363c8532fa8d052bfdd94607b3c0cd6d8
benjaminkao <54568364+benjaminkao@users.noreply.github.com>
2021-04-20 22:57:08 -0700

    Revert "Frontend/auth"

--- routes/index.js
+++ routes/index.js
@@ -6,13 +6,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/login', function(req, res, next) {
-  res.render('login', { title: 'Login' });
-});
-
-router.get('/register', function(req, res, next) {
-  res.render('register', { title: 'Register' });
-});
-
-
 module.exports = router;
